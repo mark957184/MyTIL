ca6d1d9a0e0b759d198ce9ef036040e2a9d7e889
mark957184 <marcobulsi29@gmail.com>
2026-01-17 18:55:26 +0100

    Update Print_command.py

--- Python/Print_command.py
+++ Python/Print_command.py
@@ -1,1 +1,5 @@
+# DAY 1: Print 
+# today I learned this simple yet useful command for debugging (finding where your code doesn't work), in fact it outputs what you put between commas
+# I'll watch some tutorials to introduce me into Python, so the next days i'll be ready for other commands
+
 print("Hello, IT world!")
