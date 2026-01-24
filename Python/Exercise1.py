7d7a6b74663f20ad315fa46dabeb54ff7869c53c
mark957184 <marcobulsi29@gmail.com>
2026-01-24 16:55:19 +0100

    Add files via upload
    DAY 7: first week ended, small exercise about everything I learned so far!

---
+++ Python/Exercise1.py
@@ -0,0 +1,6 @@
+'''
+DAY 7: Exercise
+First week in python, I learned plenty of things and today i'm going to exercise a little on those:
+'''
+
+# Ex: see ./Python/Exercises/main.py and ./Python/Exercises/config.py
