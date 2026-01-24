effcbf1ae498e6530d4c600ecdf7deb0ee6cec42
mark957184 <marcobulsi29@gmail.com>
2026-01-24 16:55:51 +0100

    Delete Python/Exercise1.py

--- Python/Exercise1.py
+++
@@ -1,6 +0,0 @@
-'''
-DAY 7: Exercise
-First week in python, I learned plenty of things and today i'm going to exercise a little on those:
-'''
-
-# Ex: see ./Python/Exercises/main.py and ./Python/Exercises/config.py
