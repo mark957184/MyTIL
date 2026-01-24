01fd5039ddbbb56a79c35ad4b00ec09badaf65cc
mark957184 <marcobulsi29@gmail.com>
2026-01-24 16:59:03 +0100

    Add files via upload

---
+++ Python/Exercise1/config.py
@@ -0,0 +1,5 @@
+XP_step = 10
+max_level = 5
+
+ans4 = ["leveled up!", "is grinding!", "maxed his aura!", "is stronger now!"]
+ans3 = ["Kill the dragon to earn XP! (e + Enter): ", "Help the witch to earn XP! (e + Enter): ", "Brew a potion to earn XP! (e + Enter): "]
